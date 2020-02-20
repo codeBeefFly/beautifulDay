bdec8ea148ad32700732b30381fc123d0f50c42e
codeBeefFly <lijin890119@163.com>
2020-02-20 13:17:47 +0800

    提交文件

---
+++ src/hello_world.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import cv2
+import numpy
+import scipy
+import matplotlib
+import PyQt5
+
+
+def print_hello():
+    print("hello beautiful world")
+
+
+if __name__ == "__main__":
+    print_hello()
